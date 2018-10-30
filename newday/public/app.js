704f5c92c56ea1dfdd13c8445308442abd368efb
domobritton <domonicbritton@gmail.com>
2018-10-30 15:45:58 -0700

    Change css to make things pretty

--- newday/public/app.js
+++ newday/public/app.js
@@ -26,23 +26,21 @@
             type: "line",
             data: weatherData,
             options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: false
+                },
                 scales: {
                     xAxes: [{
                         display: false //this will remove all the x-axis grid lines
                     }],
                     yAxes: [{
-                        display: false
+                        gridLines: {
+                            color: "rgba(211, 211, 211, 0.2)"
+                        }
                     }]
-                },
-                legend: {
-                    display: false
-                }
-            },
-            tooltips: {
-                callbacks: {
-                    label: (tooltipItem) => {
-                        return tooltipItem.yLabel;
-                    }
                 }
             }
         });
@@ -60,6 +58,16 @@
                 },
                 title: {
                     display: false
+                },
+                scales: {
+                    xAxes: [{
+                        display: false //this will remove all the x-axis grid lines
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: "rgba(211, 211, 211, 0.2)"
+                        }
+                    }]
                 }
             }
         });
@@ -81,16 +89,16 @@
             fill: false,
             lineTension: 0,
             backgroundColor: "rgba(52,152,219,1)",
-            borderColor: "rgba(255,255,255,1)",
+            borderColor: "rgba(211, 211, 211, 1)",
             borderCapStyle: 'butt',
             borderDash: [],
             borderDashOffset: 0.0,
             borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(255,255,255,1)",
-            pointBackgroundColor: "rgba(255,255,255,1)",
+            pointBorderColor: "rgba(211, 211, 211, 1)",
+            pointBackgroundColor: "rgba(211, 211, 211, 1)",
             pointBorderWidth: 1,
             pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(255,255,255,1)",
+            pointHoverBackgroundColor: "rgba(211, 211, 211, 1)",
             pointHoverBorderColor: "rgba(220,220,220,1)",
             pointHoverBorderWidth: 2,
             pointRadius: 1,
@@ -99,7 +107,7 @@
             spanGaps: false,
         }]
     };
-   
+
     axios.get('/getWeather').then((response => onFetchWeatherResponse(response)));
 
     function onFetchWeatherResponse(response) {
